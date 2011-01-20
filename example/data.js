ffb0927080e255af82af6c65395db8f7aa8c6123
Laurynas Butkus <laurynas.butkus@gmail.com>
2011-01-20 23:41:02 +0200

    Prepare testing data

--- example/data.js
+++ example/data.js
@@ -1,51 +1,51 @@
 var ganttData = [
 	{
-		id: 1, name: "Feature 1", series: [
-			{ name: "Planned", start: new Date(2010,00,01), end: new Date(2010,00,03) },
-			{ name: "Actual", start: new Date(2010,00,02), end: new Date(2010,00,05), color: "#f0f0f0" }
+		id: 1, name: "Facility 1", series: [
+			{ name: "Booking 1", start: new Date(2011,00,04), end: new Date(2011,00,05) },
+			{ name: "Booking 2", start: new Date(2011,00,07), end: new Date(2011,00,10), color: "#f0f0f0" }
 		]
 	}, 
 	{
-		id: 2, name: "Feature 2", series: [
-			{ name: "Planned", start: new Date(2010,00,05), end: new Date(2010,00,20) },
-			{ name: "Actual", start: new Date(2010,00,06), end: new Date(2010,00,17), color: "#f0f0f0" },
-			{ name: "Projected", start: new Date(2010,00,06), end: new Date(2010,00,17), color: "#e0e0e0" }
+		id: 2, name: "Facility 2", series: [
+			{ name: "Booking 3", start: new Date(2011,00,05), end: new Date(2011,00,20) },
+			{ name: "Booking 4", start: new Date(2011,00,21), end: new Date(2011,00,27), color: "#f0f0f0" },
+			{ name: "Booking 5", start: new Date(2011,01,06), end: new Date(2011,01,17), color: "#e0e0e0" }
 		]
 	}, 
 	{
-		id: 3, name: "Feature 3", series: [
-			{ name: "Planned", start: new Date(2010,00,11), end: new Date(2010,01,03) },
-			{ name: "Actual", start: new Date(2010,00,15), end: new Date(2010,01,03), color: "#f0f0f0" }
+		id: 3, name: "Facility 3", series: [
+			{ name: "Booking 6", start: new Date(2010,11,25), end: new Date(2011,00,03) },
+			{ name: "Booking 7", start: new Date(2011,00,15), end: new Date(2011,01,03), color: "#f0f0f0" }
 		]
 	}, 
 	{
-		id: 4, name: "Feature 4", series: [
-			{ name: "Planned", start: new Date(2010,01,01), end: new Date(2010,01,03) },
-			{ name: "Actual", start: new Date(2010,01,01), end: new Date(2010,01,05), color: "#f0f0f0" }
+		id: 4, name: "Facility 4", series: [
+			{ name: "Booking 8", start: new Date(2011,01,01), end: new Date(2011,01,03) },
+			{ name: "Booking 9", start: new Date(2011,01,04), end: new Date(2011,01,05), color: "#f0f0f0" }
 		]
 	},
 	{
-		id: 5, name: "Feature 5", series: [
-			{ name: "Planned", start: new Date(2010,02,01), end: new Date(2010,03,20) },
-			{ name: "Actual", start: new Date(2010,02,01), end: new Date(2010,03,26), color: "#f0f0f0" }
+		id: 5, name: "Facility 5", series: [
+			{ name: "Booking 10", start: new Date(2011,02,01), end: new Date(2011,03,20) },
+			{ name: "Booking 11", start: new Date(2011,03,25), end: new Date(2011,03,26), color: "#f0f0f0" }
 		]
 	}, 
 	{
-		id: 6, name: "Feature 6", series: [
-			{ name: "Planned", start: new Date(2010,00,05), end: new Date(2010,00,20) },
-			{ name: "Actual", start: new Date(2010,00,06), end: new Date(2010,00,17), color: "#f0f0f0" },
-			{ name: "Projected", start: new Date(2010,00,06), end: new Date(2010,00,20), color: "#e0e0e0" }
+		id: 6, name: "Facility 6", series: [
+			{ name: "Booking 12", start: new Date(2011,00,05), end: new Date(2011,00,10) },
+			{ name: "Booking 13", start: new Date(2011,00,14), end: new Date(2011,00,17), color: "#f0f0f0" },
+			{ name: "Booking 14", start: new Date(2011,03,06), end: new Date(2011,03,20), color: "#e0e0e0" }
 		]
 	}, 
 	{
-		id: 7, name: "Feature 7", series: [
-			{ name: "Planned", start: new Date(2010,00,11), end: new Date(2010,01,03) }
+		id: 7, name: "Facility 7", series: [
+			{ name: "Booking 15", start: new Date(2011,03,11), end: new Date(2011,04,03) }
 		]
 	}, 
 	{
-		id: 8, name: "Feature 8", series: [
-			{ name: "Planned", start: new Date(2010,01,01), end: new Date(2010,01,03) },
-			{ name: "Actual", start: new Date(2010,01,01), end: new Date(2010,01,05), color: "#f0f0f0" }
+		id: 8, name: "Facility 8", series: [
+			{ name: "Booking 16", start: new Date(2011,01,01), end: new Date(2011,01,03) },
+			{ name: "Booking 17", start: new Date(2011,01,04), end: new Date(2011,01,05), color: "#f0f0f0" }
 		]
 	}
 ];
