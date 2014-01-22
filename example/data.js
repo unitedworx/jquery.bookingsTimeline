e2b372e425a1653ee0bcc6813428588aead0f41a
Paris Paraskeva <paris@unitedworx.com>
2014-01-22 18:50:22 +0200

    update data.js bookings to use 2014 dates

--- example/data.js
+++ example/data.js
@@ -1,51 +1,51 @@
 var timelineData = [
 	{
 		id: 1, name: "Facility 1", series: [
-			{ id: 1, name: "Booking 1", start: new Date(2011,00,04), end: new Date(2011,00,05) },
-			{ id: 2, name: "Booking 2", start: new Date(2011,00,07), end: new Date(2011,00,10), color: "#f0f0f0" }
+			{ id: 1, name: "Booking 1", start: new Date(2014,00,04), end: new Date(2014,00,05) },
+			{ id: 2, name: "Booking 2", start: new Date(2014,00,07), end: new Date(2014,00,10), color: "#f0f0f0" }
 		]
 	}, 
 	{
 		id: 2, name: "Facility 2", series: [
-			{ id: 3, name: "Booking 3", start: new Date(2011,00,05), end: new Date(2011,00,20), cssClass: 'special' },
-			{ id: 4, name: "Booking 4", start: new Date(2011,00,21), end: new Date(2011,00,27), color: "#f0f0f0" },
-			{ id: 5, name: "Booking 5", start: new Date(2011,01,06), end: new Date(2011,01,17), color: "#e0e0e0" }
+			{ id: 3, name: "Booking 3", start: new Date(2014,00,05), end: new Date(2014,00,20), cssClass: 'special' },
+			{ id: 4, name: "Booking 4", start: new Date(2014,00,21), end: new Date(2014,00,27), color: "#f0f0f0" },
+			{ id: 5, name: "Booking 5", start: new Date(2014,01,06), end: new Date(2014,01,17), color: "#e0e0e0" }
 		]
 	}, 
 	{
 		id: 3, name: "Facility 3", series: [
-			{ id: 6, name: "Booking 6", start: new Date(2010,11,25), end: new Date(2011,00,03), title: "My custom title" },
-			{ id: 7, name: "Booking 7", start: new Date(2011,00,15), end: new Date(2011,01,03), color: "#f0f0f0" }
+			{ id: 6, name: "Booking 6", start: new Date(2013,11,25), end: new Date(2014,00,03), title: "My custom title" },
+			{ id: 7, name: "Booking 7", start: new Date(2014,00,15), end: new Date(2014,01,03), color: "#f0f0f0" }
 		]
 	}, 
 	{
 		id: 4, name: "Facility 4", series: [
-			{ id: 8, name: "Booking 8", start: new Date(2011,01,01), end: new Date(2011,01,03), cssClass: 'special' },
-			{ id: 9, name: "Booking 9", start: new Date(2011,01,04), end: new Date(2011,01,05), color: "#f0f0f0" }
+			{ id: 8, name: "Booking 8", start: new Date(2014,01,01), end: new Date(2014,01,03), cssClass: 'special' },
+			{ id: 9, name: "Booking 9", start: new Date(2014,01,04), end: new Date(2014,01,05), color: "#f0f0f0" }
 		]
 	},
 	{
 		id: 5, name: "Facility 5", series: [
-			{ id: 10, name: "Booking 10", start: new Date(2011,02,01), end: new Date(2011,03,20) },
-			{ id: 11, name: "Booking 11", start: new Date(2011,03,25), end: new Date(2011,03,26), color: "#f0f0f0" }
+			{ id: 10, name: "Booking 10", start: new Date(2014,02,01), end: new Date(2014,03,20) },
+			{ id: 11, name: "Booking 11", start: new Date(2014,03,25), end: new Date(2014,03,26), color: "#f0f0f0" }
 		]
 	}, 
 	{
 		id: 6, name: "Facility 6", series: [
-			{ id: 12, name: "Booking 12", start: new Date(2011,00,05), end: new Date(2011,00,10) },
-			{ id: 13, name: "Booking 13", start: new Date(2011,00,14), end: new Date(2011,00,17), color: "#f0f0f0" },
-			{ id: 14, name: "Booking 14", start: new Date(2011,03,06), end: new Date(2011,03,20), color: "#e0e0e0" }
+			{ id: 12, name: "Booking 12", start: new Date(2014,00,05), end: new Date(2014,00,10) },
+			{ id: 13, name: "Booking 13", start: new Date(2014,00,14), end: new Date(2014,00,17), color: "#f0f0f0" },
+			{ id: 14, name: "Booking 14", start: new Date(2014,03,06), end: new Date(2014,03,20), color: "#e0e0e0" }
 		]
 	}, 
 	{
 		id: 7, name: "Facility 7", series: [
-			{ id: 15, name: "Booking 15", start: new Date(2011,03,11), end: new Date(2011,04,03) }
+			{ id: 15, name: "Booking 15", start: new Date(2014,03,11), end: new Date(2014,04,03) }
 		]
 	}, 
 	{
 		id: 8, name: "Facility 8", series: [
-			{ id: 16, name: "Booking 16", start: new Date(2011,01,01), end: new Date(2011,01,03) },
-			{ id: 17, name: "Booking 17", start: new Date(2011,01,04), end: new Date(2011,01,05), color: "#f0f0f0" }
+			{ id: 16, name: "Booking 16", start: new Date(2014,01,01), end: new Date(2014,01,03) },
+			{ id: 17, name: "Booking 17", start: new Date(2014,01,04), end: new Date(2014,01,05), color: "#f0f0f0" }
 		]
 	},
 	{
